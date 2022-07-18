bb12624ab1200ce7e49465fec1a091e10cc51af9
Minhvu6533 <minhvv@vtvlive.vn>
2022-07-18 16:00:56 +0700

    create

--- Sources/ios_sdk/SDK.swift
+++ Sources/ios_sdk/SDK.swift
@@ -9,16 +9,15 @@ import Foundation
 
 public struct SDK {
     
-    static let shared = SDK();
+    public static let shared = SDK();
     
     public private(set) var text = "Hello, World!"
     
     
-
     public init() {
     }
     
-    func login(){
+    public func login(){
         print("Login  sdk")
     }
 }

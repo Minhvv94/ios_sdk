7516538b2db26be206542c5ce99e4cbd8b120ace
Minhvu6533 <minhvv@vtvlive.vn>
2022-07-18 17:03:46 +0700

    create login view

--- Package.swift
+++ Package.swift
@@ -6,10 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ios_sdk",
     platforms: [
-            .iOS(.v12),
-            .tvOS(.v12),
-            .watchOS(.v5),
-            .macOS(.v10_15)
+            .iOS(.v12)
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
---
+++ Sources/ios_sdk/Controller/LoginView.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Minh Vu on 18/07/2022.
+//
+
+import Foundation
+import UIKit
+
+class LoginView: UIView {
+    
+}

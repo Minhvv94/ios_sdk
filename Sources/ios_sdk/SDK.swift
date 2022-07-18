d026cce97c5dae9436546741bc3a4bda0d37d46c
Minhvu6533 <minhvv@vtvlive.vn>
2022-07-18 15:50:10 +0700

    create sdk class

---
+++ Sources/ios_sdk/SDK.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Minh Vu on 18/07/2022.
+//
+
+import Foundation
+
+public struct SDK {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func login(){
+        print("Login  sdk")
+    }
+}
+

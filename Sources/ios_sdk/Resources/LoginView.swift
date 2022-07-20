478ffd6b3a94c5bd6c08b8e087f3c8e9fb8e8f4c
Minhvu6533 <minhvv@vtvlive.vn>
2022-07-20 17:58:19 +0700

    update

--- Sources/ios_sdk/Resources/LoginView.swift
+++ Sources/ios_sdk/Resources/LoginView.swift
@@ -1,14 +1,13 @@
 //
-//  File.swift
+//  LoginView.swift
 //  
 //
-//  Created by Minh Vu on 19/07/2022.
+//  Created by Minh Vu on 20/07/2022.
 //
 
-import Foundation
 import UIKit
 
-class LoginView : UIView {
-        
+class LoginView: UIView {
+
 
 }

7f0ce9c11719c103aa24062fa44a75683e38aeed
Minhvu6533 <minhvv@vtvlive.vn>
2022-07-20 17:04:30 +0700

    addSubview loginview

--- Sources/ios_sdk/Resources/UIView.swift
+++ Sources/ios_sdk/Resources/UIView.swift
@@ -12,7 +12,8 @@ extension UIView {
     public class func fromNib(named: String? = nil) -> Self {
         let name = named ?? "\(Self.self)"
         guard
-            let nib = Bundle.main.loadNibNamed(name, owner: nil, options: nil)
+            //let nib = Bundle.main.loadNibNamed(name, owner: nil, options: nil)
+            let nib = Bundle.module.loadNibNamed(name, owner: nil, options: nil)
             else { fatalError("missing expected nib named: \(name)") }
         guard
             /// we're using `first` here because compact map chokes compiler on

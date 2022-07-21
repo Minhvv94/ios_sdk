8f445f1ee108d266141ec524a5287eee7df6fe3b
Minhvu6533 <minhvv@vtvlive.vn>
2022-07-21 11:34:22 +0700

    update

---
+++ Sources/ios_sdk/SDKObjectiveC.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Minh Vu on 21/07/2022.
+//
+
+import Foundation
+
+protocol SwiftProtocol {
+    func login()
+}
+
+@objc class SDKObjectiveC: NSObject, SwiftProtocol {
+    
+    @objc func login(){
+        print("call SDKObjectiveC framework ")
+    }
+}

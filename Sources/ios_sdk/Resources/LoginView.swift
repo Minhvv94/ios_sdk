9028295c7b975647926f5a08f55271b53292e831
Minhvu6533 <minhvv@vtvlive.vn>
2022-07-25 11:01:42 +0700

    update

--- Sources/ios_sdk/Resources/LoginView.swift
+++ Sources/ios_sdk/Resources/LoginView.swift
@@ -7,7 +7,17 @@
 
 import UIKit
 
+
+@objc(LoginView)
 class LoginView: UIView {
 
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    @objc init(frame: CGRect, editAccess: Int) {
+        super.init(frame:frame)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }

1041ef6225973aac4d5be456a0eb9e3f76317ea1
Minhvu6533 <minhvv@vtvlive.vn>
2022-07-25 17:55:08 +0700

    update

--- Sources/ios_sdk/Resources/LoginView.swift
+++ Sources/ios_sdk/Resources/LoginView.swift
@@ -11,13 +11,25 @@ import UIKit
 @objc(LoginView)
 class LoginView: UIView {
 
+    @IBOutlet weak var contentView: UIView!
+    //@IBOutlet var contentView: UIView!
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUpView()
     }
     @objc init(frame: CGRect, editAccess: Int) {
         super.init(frame:frame)
+        setUpView()
     }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        setUpView()
+    }
+    
+    private func setUpView(){
+        Bundle.module.loadNibNamed("LoginView", owner: self, options: nil)
+        self.addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight,.flexibleWidth]
     }
 }

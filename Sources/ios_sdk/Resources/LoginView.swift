ae248f6b53490e59f18921247dc5319884c460e1
Minhvu6533 <minhvv@vtvlive.vn>
2022-07-26 09:31:55 +0700

    update

--- Sources/ios_sdk/Resources/LoginView.swift
+++ Sources/ios_sdk/Resources/LoginView.swift
@@ -13,19 +13,20 @@ class LoginView: UIView {
 
     @IBOutlet var contentView: UIView!
     
+    @IBOutlet weak var txtPass: UITextField!
+    @IBOutlet weak var txtUserName: UITextField!
     @IBAction func btnClose(_ sender: Any) {
         contentView.removeFromSuperview();
         
     }
-    //@IBOutlet var contentView: UIView!
+    @IBAction func btnLogin(_ sender: Any) {
+        print("user name : "\(txtUserName))
+        print("password : "\(txtPass))
+    }
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUpView()
     }
-//    @objc init(frame: CGRect, editAccess: Int) {
-//        super.init(frame:frame)
-//        setUpView()
-//    }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setUpView()

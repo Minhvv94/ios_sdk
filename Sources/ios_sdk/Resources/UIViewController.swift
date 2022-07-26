d382c5359dc1ed87f7546cfc71cd152fdf70ccbd
Minhvu6533 <minhvv@vtvlive.vn>
2022-07-26 11:40:58 +0700

    update

--- Sources/ios_sdk/Resources/UIViewController.swift
+++ Sources/ios_sdk/Resources/UIViewController.swift
@@ -5,16 +5,16 @@
 //  Created by Minh Vu on 26/07/2022.
 //
 
-import Foundation
-import UIKit
-extension UIViewController {
-    func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-    
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-}
+//import Foundation
+//import UIKit
+//extension UIViewController {
+//    func hideKeyboardWhenTappedAround() {
+//        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+//        tap.cancelsTouchesInView = false
+//        view.addGestureRecognizer(tap)
+//    }
+//    
+//    @objc func dismissKeyboard() {
+//        view.endEditing(true)
+//    }
+//}

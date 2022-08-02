533372b6083db2fb2115b8890daa8c3236e8bbb3
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-02 15:38:59 +0700

    update

--- Sources/ios_sdk/Core/User/AppInfo.swift
+++ Sources/ios_sdk/Core/User/AppInfo.swift
@@ -9,24 +9,24 @@ import Foundation
 @objc(AppInfo)
 public class AppInfo :NSObject{
     
-    @objc static let shared = AppInfo()
-    @objc var firebaseConfigFile : String=""
-    @objc var client_id : String=""
-    @objc var client_secret : String=""
-    @objc var platformOS : String=""
-    @objc var packageId : String=""
-    @objc var version : String=""
+    @objc public static let shared = AppInfo()
+    @objc public var firebaseConfigFile : String=""
+    @objc public var client_id : String=""
+    @objc public var client_secret : String=""
+    @objc public var platformOS : String=""
+    @objc public var packageId : String=""
+    @objc public var version : String=""
     /*Information about appsflyer */
-    @objc var afDevKey : String=""
-    @objc var afAppId : String=""
-    @objc var googleWebClient : String=""
-    @objc var locale : String=""
-    @objc var hotLinkHomepage : String=""
-    @objc var hotLinkFanpage : String=""
-    @objc var hotLinkGroup : String=""
-    @objc var hotLinkChat : String=""
-    @objc var hotline : String=""
-    @objc var achievedLevels: NSArray=[]
-    @objc var achievedVips: NSArray=[]
+    @objc public var afDevKey : String=""
+    @objc public var afAppId : String=""
+    @objc public var googleWebClient : String=""
+    @objc public var locale : String=""
+    @objc public var hotLinkHomepage : String=""
+    @objc public var hotLinkFanpage : String=""
+    @objc public var hotLinkGroup : String=""
+    @objc public var hotLinkChat : String=""
+    @objc public var hotline : String=""
+    @objc public var achievedLevels: NSArray=[]
+    @objc public var achievedVips: NSArray=[]
     
 }

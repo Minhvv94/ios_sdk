aea8858f2d3daef1b5f52a316d2a916c09775f92
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-02 16:36:13 +0700

    update

--- Sources/ios_sdk/SdkIplement.swift
+++ Sources/ios_sdk/SdkIplement.swift
@@ -6,9 +6,20 @@
 //
 
 import Foundation
+import UIKit
 
-@objc
-public class SdkIplement: NSObject, SdkI {
+@objc(SdkIplement)
+public class SdkIplement: UIView, SdkI {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    @objc init(frame: CGRect, editAccess: Int) {
+        super.init(frame:frame)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     @objc func loginTest() {
         print("login")
     }

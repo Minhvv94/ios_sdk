5acad321467280f8606b2cf14624f7c4bd6d9237
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-03 17:32:02 +0700

    no message

--- Sources/ios_sdk/Resources/LoginView.swift
+++ Sources/ios_sdk/Resources/LoginView.swift
@@ -74,18 +74,7 @@ class LoginView: UIView {
 //        let client = AppInfo.shared.client_id
 //        print("client   : \(client)")
         
-        if(AppInfo.shared != nil && AppInfo.shared.packageId != nil){
-            let version = AppInfo.shared.version == "" ? "1.0": AppInfo.shared.version;
-            
-            Repository().getGameCheckMaintain(request_app_package: AppInfo.shared.packageId, request_channel: AppInfo.shared.platformOS, request_version: AppInfo.shared.version, callBack: { (response) in
-                if response.isSuccess(){
-                    let data = JsonParserManager.getMaitainCheckerJson(jsonString: response.rawData ?? "")
-                    print("message : \(data?.data?.messageMaintain)")
-                }
-            })
-            
-            
-        }
+        
         
         
     }
--- Sources/ios_sdk/SDKObjectiveC.swift
+++ Sources/ios_sdk/SDKObjectiveC.swift
@@ -24,13 +24,34 @@ public class SDKObjectiveC: UIView {
     @objc open func  login() {
         if let topVC = UIApplication.topViewController() {
             
-            let screenFrame = topVC.view.frame
-            let windowWidth = screenFrame.width
-            let windowHeight = screenFrame.height
-            let rect = CGRect(x: 0, y: 0, width: windowWidth , height: windowHeight)
-            let newView = LoginView(frame: rect)
-            newView.tag = 100
-            topVC.view.addSubview(newView)
+//            let screenFrame = topVC.view.frame
+//            let windowWidth = screenFrame.width
+//            let windowHeight = screenFrame.height
+//            let rect = CGRect(x: 0, y: 0, width: windowWidth , height: windowHeight)
+//            let newView = LoginView(frame: rect)
+//            newView.tag = 100
+//            topVC.view.addSubview(newView)
+            
+            
+            
+            if(AppInfo.shared != nil && AppInfo.shared.packageId != nil){
+                let version = AppInfo.shared.version == "" ? "1.0": AppInfo.shared.version;
+                
+                Repository().getGameCheckMaintain(request_app_package: AppInfo.shared.packageId, request_channel: AppInfo.shared.platformOS, request_version: AppInfo.shared.version, callBack: { (response) in
+                    if response.isSuccess(){
+                        let data = JsonParserManager.getMaitainCheckerJson(jsonString: response.rawData ?? "")
+                        if (data?.data?.isMaintained)! {
+                            let alert = UIAlertController(title: "Thông báo", message: data?.data?.messageMaintain, preferredStyle: UIAlertController.Style.alert)
+
+                                    // add an action (button)
+                                    alert.addAction(UIAlertAction(title: "Đồng ý", style: UIAlertAction.Style.default, handler: nil))
+
+                                    // show the alert
+                            topVC.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                })
+            }
         }
         print("login")
     }

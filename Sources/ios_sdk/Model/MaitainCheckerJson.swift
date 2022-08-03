4dff76fefe6163ce5191f44bd0e2f48bbc5ad5ed
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-03 16:36:41 +0700

    no message

--- Sources/ios_sdk/Model/MaitainCheckerJson.swift
+++ Sources/ios_sdk/Model/MaitainCheckerJson.swift
@@ -10,8 +10,25 @@ import Foundation
 class MaitainCheckerJson: Codable {
     let isMaintained: Bool?
     let messageMaintain: String?
-
     let enableGoogleLogin: Bool?
     let enableFacebookLogin: Bool?
     let enableAppleLogin: Bool?
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case isMaintained = "isMaintained"
+        case messageMaintain = "messageMaintain"
+        case messenableGoogleLoginage = "messenableGoogleLoginage"
+        case enableFacebookLogin = "enableFacebookLogin"
+        case enableAppleLogin = "enableAppleLogin"
+        
+    }
+    
+    init(isMaintained: Bool?, messageMaintain: String?, messenableGoogleLoginage: Bool?, enableFacebookLogin: Bool?, enableAppleLogin: Bool?) {
+        self.isMaintained = isMaintained
+        self.messageMaintain = messageMaintain
+        self.messenableGoogleLoginage = messenableGoogleLoginage
+        self.enableFacebookLogin = enableFacebookLogin
+        self.enableAppleLogin = enableAppleLogin
+    }
 }

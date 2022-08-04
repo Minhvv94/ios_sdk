4dd70bc65bc543ceff393a998d3578fe296306c5
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-04 16:47:14 +0700

    no message

--- Sources/ios_sdk/SDKObjectiveC.swift
+++ Sources/ios_sdk/SDKObjectiveC.swift
@@ -23,9 +23,8 @@ public class SDKObjectiveC: UIView {
     private let buttonView: FloatingButtonView = {
         let button = FloatingButtonView()
         
-        if let path = Bundle.module.path(forResource: "BtnDashboard", ofType: "png"),  let c = UIImage(contentsOfFile: path) {
+        if let path = Bundle.module.path(forResource: "BtnDashboard", ofType: "png"),  let image = UIImage(contentsOfFile: path) {
             print("\(path)")
-            print("\(image)")
 //            let imageName = Bundle.module.path(forResource: "BtnDashboard", ofType: "png")
 //            let image = UIImage(named: imageName!)
             let imageView = UIImageView(image: image)

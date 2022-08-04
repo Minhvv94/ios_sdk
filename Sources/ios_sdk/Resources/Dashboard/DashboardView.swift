5f83474c631c851a04b0ddadedd21f7758406888
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-04 15:37:54 +0700

    no message

--- Sources/ios_sdk/Resources/Dashboard/DashboardView.swift
+++ Sources/ios_sdk/Resources/Dashboard/DashboardView.swift
@@ -13,6 +13,10 @@ class DashboardView: UIView {
     
     @IBOutlet var contentView: UIView!
     
+    @IBOutlet weak var btnBack: UIView!
+    
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUpView()
@@ -21,11 +25,22 @@ class DashboardView: UIView {
         super.init(coder: aDecoder)
     }
     
+    
+    
     private func setUpView(){
         Bundle.module.loadNibNamed("DashboardView", owner: self, options: nil)
         self.addSubview(contentView)
         contentView.frame = self.bounds
         contentView.autoresizingMask = [.flexibleHeight,.flexibleWidth]
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+                
+                // 2. add the gesture recognizer to a view
+        btnBack.addGestureRecognizer(tapGesture)
     
     }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+            print("click ")
+   }
 }

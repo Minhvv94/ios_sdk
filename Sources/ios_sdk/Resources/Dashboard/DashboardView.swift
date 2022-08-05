e44d6da7d01bc926bd7057edca6f8ca2427b030d
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-05 12:04:10 +0700

    no message

--- Sources/ios_sdk/Resources/Dashboard/DashboardView.swift
+++ Sources/ios_sdk/Resources/Dashboard/DashboardView.swift
@@ -37,6 +37,17 @@ class DashboardView: UIView {
                 
                 // 2. add the gesture recognizer to a view
         btnBack.addGestureRecognizer(tapGesture)
+        
+        self.overrideUserInterfaceStyle = .light
+        self.backgroundColor = .white
+        self.addSubview(viewPager)
+        
+        NSLayoutConstraint.activate([
+            viewPager.widthAnchor.constraint(equalTo: self.widthAnchor),
+            viewPager.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.7),
+            viewPager.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            viewPager.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor)
+        ])
     
     }
     
@@ -45,4 +56,39 @@ class DashboardView: UIView {
             self.removeFromSuperview()
         }
    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        self.navigationItem.title = "ViewPager"
+//
+//        self.navigationController?.navigationBar.standardAppearance.backgroundColor = .systemBlue
+//        self.navigationController?.navigationBar.standardAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+//    }
+    lazy var viewPager: ViewPager = {
+        let viewPager = ViewPager(
+            tabSizeConfiguration: .fillEqually(height: 60, spacing: 0)
+        )
+        let view1 = UIView()
+        view1.backgroundColor = .red
+        
+        let view2 = UIView()
+        view2.backgroundColor = .blue
+        
+        let view3 = UIView()
+        view3.backgroundColor = .orange
+        
+        viewPager.tabbedView.tabs = [
+            AppTabItemView(title: "First"),
+            AppTabItemView(title: "Second"),
+            AppTabItemView(title: "Third")
+        ]
+        viewPager.pagedView.pages = [
+            view1,
+            view2,
+            view3
+        ]
+        viewPager.translatesAutoresizingMaskIntoConstraints = false
+        return viewPager
+    }()
 }

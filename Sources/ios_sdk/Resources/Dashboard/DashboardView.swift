fc78a33095ad32c9c830b6f084404dd3fc8eaab0
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-08 12:11:57 +0700

    no message

--- Sources/ios_sdk/Resources/Dashboard/DashboardView.swift
+++ Sources/ios_sdk/Resources/Dashboard/DashboardView.swift
@@ -38,16 +38,34 @@ class DashboardView: UIView {
         // 2. add the gesture recognizer to a view
         btnBack.addGestureRecognizer(tapGesture)
         
-        mainView.overrideUserInterfaceStyle = .light
-        mainView.backgroundColor = .orange
-        mainView.addSubview(viewPager)
-
-        NSLayoutConstraint.activate([
-            viewPager.leftAnchor.constraint(equalTo: mainView.leftAnchor),
-            viewPager.rightAnchor.constraint(equalTo: mainView.rightAnchor),
-            viewPager.topAnchor.constraint(equalTo: mainView.topAnchor),
-            viewPager.bottomAnchor.constraint(equalTo: mainView.bottomAnchor)
-        ])
+        
+        
+        let vc1 = UIViewController()
+        let vc2 = UIViewController()
+        tabItems = [(vc1, "First"), (vc2, "Second")]
+        mainView.tabBackgroundColor = UIColor.green
+        mainView.tabWidth = view.frame.width / CGFloat(tabItems.count)
+        mainView.hidesTopViewOnSwipeType = .all
+//        let segmentedControl = HMSegmentedControl(sectionTitles: [
+//            "Trending",
+//            "News",
+//            "Library"
+//        ])
+//
+//        segmentedControl.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
+//        segmentedControl.addTarget(self, action: #selector(segmentedControlChangedValue(segmentedControl:)), for: .valueChanged)
+//        view.addSubview(segmentedControl)
+        
+//        mainView.overrideUserInterfaceStyle = .light
+//        mainView.backgroundColor = .orange
+//        mainView.addSubview(viewPager)
+//
+//        NSLayoutConstraint.activate([
+//            viewPager.leftAnchor.constraint(equalTo: mainView.leftAnchor),
+//            viewPager.rightAnchor.constraint(equalTo: mainView.rightAnchor),
+//            viewPager.topAnchor.constraint(equalTo: mainView.topAnchor),
+//            viewPager.bottomAnchor.constraint(equalTo: mainView.bottomAnchor)
+//        ])
     
     }
     
@@ -56,25 +74,25 @@ class DashboardView: UIView {
             self.removeFromSuperview()
         }
    }
-    lazy var viewPager: ViewPager = {
-        let viewPager = ViewPager(
-            tabSizeConfiguration: .fillEqually(height: 60, spacing: 0)
-        )
-        let view1 = UIView()
-        view1.backgroundColor = .red
-
-        let view2 = UIView()
-        view2.backgroundColor = .blue
-
-        viewPager.tabbedView.tabs = [
-            AppTabItemView(title: "First"),
-            AppTabItemView(title: "Second")
-        ]
-        viewPager.pagedView.pages = [
-            view1,
-            view2
-        ]
-        viewPager.translatesAutoresizingMaskIntoConstraints = false
-        return viewPager
-    }()
+//    lazy var viewPager: ViewPager = {
+//        let viewPager = ViewPager(
+//            tabSizeConfiguration: .fillEqually(height: 60, spacing: 0)
+//        )
+//        let view1 = UIView()
+//        view1.backgroundColor = .red
+//
+//        let view2 = UIView()
+//        view2.backgroundColor = .blue
+//
+//        viewPager.tabbedView.tabs = [
+//            AppTabItemView(title: "First"),
+//            AppTabItemView(title: "Second")
+//        ]
+//        viewPager.pagedView.pages = [
+//            view1,
+//            view2
+//        ]
+//        viewPager.translatesAutoresizingMaskIntoConstraints = false
+//        return viewPager
+//    }()
 }

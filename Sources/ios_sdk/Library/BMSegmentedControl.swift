1869b118c9042a0dea4a7e8a74b8186cf9dc00d7
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-17 18:16:12 +0700

    no message

--- Sources/ios_sdk/Library/BMSegmentedControl.swift
+++ Sources/ios_sdk/Library/BMSegmentedControl.swift
@@ -306,7 +306,7 @@ class BMSegmentedControl: UIControl {
             labelFrame.origin.y = 4
             labelFrame.size.width = self.selectedLabel.frame.width
             labelFrame.size.height = self.bounds.height
-            let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.thumbView.bounds.height, height: 5))
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.thumbView.bounds.width, height: 5))
             label.backgroundColor = UIColor.white
             self.thumbView.addSubview(label)
             self.thumbView.frame = self.setDefaultSelectionPoint(self.selectedIndex)

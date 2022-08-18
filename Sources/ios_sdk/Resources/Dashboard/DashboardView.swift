15ee71c97dba5b847a3d2a2cc31c7e9e6bdee2c4
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-18 10:44:10 +0700

    no message

--- Sources/ios_sdk/Resources/Dashboard/DashboardView.swift
+++ Sources/ios_sdk/Resources/Dashboard/DashboardView.swift
@@ -99,7 +99,7 @@ class DashboardView: UIView {
         let windowWidth = contentView.bounds.width
         let windowHeight = contentView.bounds.height
         
-        let rect = CGRect(x: 0, y: 0, width: windowWidth* 0.4, height: windowHeight)
+        let rect = CGRect(x: 0, y: 0, width: windowWidth, height: windowHeight)
         let personalHeaderView = PersonalHeaderView(frame: rect)
         personalHeaderView.tag = 110
         contentView.addSubview(personalHeaderView)

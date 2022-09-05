121f76bb5385e79fd2467cfb224bcfb3129429ca
Minhvu6533 <minhvv@vtvlive.vn>
2022-09-05 14:47:27 +0700

    no message

--- Sources/ios_sdk/OptionalMethodsProtocol.swift
+++ Sources/ios_sdk/OptionalMethodsProtocol.swift
@@ -14,3 +14,13 @@ protocol OptionalMethodsProtocol {
     func didChooseItem(at index: Int) // Required
     @objc func didDismiss() // Optional
 }
+
+extension OptionalMethodsProtocol {
+    func didChooseItem(at index: Int) {
+        print("OK, let's go!")
+    }
+    
+    func didDismiss(){
+        print("OK, let's go! didDismiss")
+    }
+}

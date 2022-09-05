775acf83441b0326c54916dd21f77fb875904a17
Minhvu6533 <minhvv@vtvlive.vn>
2022-09-05 17:31:09 +0700

    no message

--- Sources/ios_sdk/Resources/Login/LoginView.swift
+++ Sources/ios_sdk/Resources/Login/LoginView.swift
@@ -69,17 +69,6 @@ class LoginView: UIView {
     }
     @IBAction func btnPlayNow(_ sender: Any) {
         // get device ID
-        
-//        let deviceId = Utils.shared.getUUID()
-//        let clentId = UserDefaults.standard.string(forKey: AppInfo.KEY_CLIENT_ID)  ?? ""
-//        print("client id  : \(clentId)")
-//
-//        let client = AppInfo.shared.client_id
-//        print("client   : \(client)")
-        
-        
-        
-        
     }
     
     

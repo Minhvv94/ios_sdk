d32449e7ee27841931c265fb880281ea9c407855
Minhvu6533 <minhvv@vtvlive.vn>
2022-09-09 11:49:56 +0700

    no message

--- Sources/ios_sdk/Resources/Login/LoginView.swift
+++ Sources/ios_sdk/Resources/Login/LoginView.swift
@@ -80,7 +80,7 @@ class LoginView: UIView {
                         let windowWidth = screenFrame.width
                         let windowHeight = screenFrame.height
                         let rect = CGRect(x: 0, y: 0, width: windowWidth , height: windowHeight)
-                        let welcomeView = WelcomeView(frame: rect)
+                        let welcomeView = TestView(frame: rect)
 //                        welcomeView.sayHi(accountName: "MinhVu")
                         welcomeView.tag = 107
                         topVC.view.addSubview(welcomeView)
